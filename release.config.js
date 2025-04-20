cde67e35e9e8351bdb692310255a276ce8acda62
nachatz <nik.achatz@gmail.com>
2025-04-19 23:51:14 -0700

    chore: remove unneeded checks

--- release.config.js
+++ release.config.js
@@ -3,7 +3,14 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/github',
     '@semantic-release/git',
+    '@semantic-release/github',
+    [
+      '@semantic-release/github',
+      {
+        successComment: false,
+        failComment: false,
+      },
+    ],
   ],
 };
