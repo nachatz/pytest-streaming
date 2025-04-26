c50b5215a82c9a681a551adcbbed27b6171600aa
nachatz <nik.achatz@gmail.com>
2025-04-25 23:34:36 -0700

    encapsulate

--- pytest_streaming/plugin.py
+++ pytest_streaming/plugin.py
@@ -8,11 +8,11 @@ from pytest import OptionGroup
 from pytest import Parser
 from pytest import Session
 
-from pytest_streaming.config import Configuration
-from pytest_streaming.config import Defaults
 from pytest_streaming.config import MarkerConfiguration
 from pytest_streaming.fixtures.markers import pubsub_setup_marker
-from pytest_streaming.pubsub.publisher import GCPPublisher
+from pytest_streaming.pubsub.plugin import pubsub_addoption
+from pytest_streaming.pubsub.plugin import pubsub_sessionfinish
+from pytest_streaming.pubsub.plugin import pubsub_sessionstart
 
 
 def pytest_addoption(parser: Parser) -> None:
@@ -24,31 +24,16 @@ def pytest_addoption(parser: Parser) -> None:
     Args:
         parser: pytest parser object
     """
-    _: OptionGroup = parser.getgroup("pytest-streaming", "Pub/Sub plugin options")
-
-    parser.addini(
-        Configuration.PUBSUB_GLOBAL_TOPICS,
-        "Comma separated list of global Pub/Sub topics to create at session start - default is None and is a line list",
-        type="linelist",
-        default=[],
-    )
-
-    parser.addini(
-        Configuration.PUBSUB_GLOBAL_DELETE,
-        "Whether to delete global topics after session finishes (True/False) - default is True",
-        type="bool",
-        default=False,
-    )
-
-    parser.addini(
-        Configuration.PUBSUB_PROJECT_ID,
-        "GCP project ID to use for Pub/Sub topics (local only) - default is 'default'",
-        type="string",
-        default=Defaults.PROJECT_ID,
-    )
+    _: OptionGroup = parser.getgroup("pytest-streaming", "Streaming plugin options")
+    pubsub_addoption(parser)
 
 
 def pytest_configure(config: Config) -> None:
+    """Establish all of our marker setups.
+
+    Args:
+        config: pytest config object
+    """
     config.addinivalue_line(
         "markers", f"{MarkerConfiguration.pubsub}: Create specified Pub/Sub topics automatically for the test. "
     )
@@ -60,14 +45,7 @@ def pytest_sessionstart(session: Session) -> None:
     Args:
         session: pytest session object
     """
-    config: Config = session.config
-    global_topics_to_create = config.getini(Configuration.PUBSUB_GLOBAL_TOPICS)
-    project_id = config.getini(Configuration.PUBSUB_PROJECT_ID)
-
-    if not global_topics_to_create:
-        return
-
-    GCPPublisher().setup_testing_topics(project_id, global_topics_to_create)
+    pubsub_sessionstart(session)
 
 
 def pytest_sessionfinish(session: Session, exitstatus: int) -> None:
@@ -77,16 +55,7 @@ def pytest_sessionfinish(session: Session, exitstatus: int) -> None:
         session: pytest session object
         exitstatus: exit status of the session
     """
-
-    config: Config = session.config
-    global_topics_to_create = config.getini(Configuration.PUBSUB_GLOBAL_TOPICS)
-    cleanup_global_topics = config.getini(Configuration.PUBSUB_GLOBAL_DELETE)
-    project_id = config.getini(Configuration.PUBSUB_PROJECT_ID)
-
-    if not cleanup_global_topics:
-        return
-
-    GCPPublisher().delete_testing_topics(project_id, global_topics_to_create)
+    pubsub_sessionfinish(session)
 
 
 @pytest.fixture(autouse=True)
