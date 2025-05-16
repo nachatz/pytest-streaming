c653511befcb35418d8c34baa74aa8d98fdd7d0c
nachatz <nik.achatz@gmail.com>
2025-05-15 21:07:42 -0700

    rlease

--- release.config.js
+++ release.config.js
@@ -3,7 +3,16 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/git',
+    "semantic-release-pypi",
+    [
+      "@semantic-release/git",
+      {
+        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+        "assets": [
+          "pyproject.toml"
+        ]
+      }
+    ],
     [
       '@semantic-release/github',
       {
